3dbf95370862743baeb0ec2f579334c932a4e7da
Deinyefa Eporwei <df.eporwei@gmail.com>
2017-04-02 16:45:12 -0400

    set time limit for game to wrap up the level

---
+++ Assets/Archery/Scripts/TimeLeft.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimeLeft : MonoBehaviour {
+
+    private Text timeLeftText;
+
+    public float gameDuration = 10f;
+
+	// Use this for initialization
+	void Start () {
+        timeLeftText = GetComponent<Text>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        float timeLeft;
+
+        timeLeft = Time.time;
+        timeLeftText.text = timeLeft.ToString();
+
+        if (timeLeft > gameDuration)
+            Time.timeScale = 0;
+        if (timeLeft > gameDuration / 2)
+            timeLeftText.color = Color.red;
+    }
+}
